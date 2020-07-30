fa0beca63be92d4467ffc8a0e7ba1c4bf4f8081e
Ashwin Pandey <ashwin.pandey11197@gmail.com>
2020-07-30 16:19:20 +0530

    Variables

--- Variable/Variable/Program.cs
+++ Variable/Variable/Program.cs
@@ -21,35 +21,3 @@ namespace Variable
         }
     }
 }
-
-
-/*
- * README
- * 
- * What is a variable?
- * A varaible is representation of information.
- * 
- * In order to access and process an information, it needs to be stored, that's where variables come in.
- * It is used for storing information.
- * 
- * In C# there are different types of variable, we need to specify variable types, 
- * one variable type will not necessarily be compatible with another variable type.
- * 
- * Variable types have reserved keywords which canot be used for any other purposes.
- * Type tells us what kind of data it will store,
- * for example, 
- * "int" will store integer data (numbers), 
- * "string" allows us to store textual information (string myName = "Ashwin"),
- * "bool" allows us to store boolean values ("true/false" OR "0/1")
- * 
- * Variable names should be meaningful, which identifies the purpose of that variable,
- * for example, storing count of the number of books, then the variable should be "countOfBooks".
- * 
- * We can convert variable types,
- * for example,
- * string number = "2";
- * int numberConverted = Convert.ToInt32(number);
- * 
- * Console.Writeline() can be used to print the variable values in console.
- * 
- */
---
+++ Variable/Variable/README.md
@@ -0,0 +1,27 @@
+# Variables
+
+### What is a variable?
+A varaible is representation of information.
+<br>
+In order to access and process an information, it needs to be stored, that's where variables come in.
+It is used for storing information.
+<br>
+In C# there are different types of variable, we need to specify variable types, 
+one variable type will not necessarily be compatible with another variable type.
+<br>
+Variable types have reserved keywords which canot be used for any other purposes.<br>
+Type tells us what kind of data it will store,<br>
+for example, <br>
+"int" will store integer data (numbers), <br>
+"string" allows us to store textual information (string myName = "Ashwin"),<br>
+"bool" allows us to store boolean values ("true/false" OR "0/1")<br>
+<br>
+Variable names should be meaningful, which identifies the purpose of that variable,<br>
+for example, storing count of the number of books, then the variable should be "countOfBooks".<br>
+<br>
+We can convert variable types,<br>
+for example,<br>
+string number = "2";<br>
+int numberConverted = Convert.ToInt32(number);<br>
+<br>
+Console.Writeline() can be used to print the variable values in console.
